9ccad11fafe7ec1d01c49d2d423d81f7a28bbae1
carlsond2 <135249731+carlsond2@users.noreply.github.com>
2023-06-06 01:23:56 -0400

    fixed the prompt

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -10,30 +10,46 @@ export default async function (req, res) {
     res.status(500).json({
       error: {
         message: "OpenAI API key not configured, please follow instructions in README.md",
-      }
+      },
     });
     return;
   }
 
-  const theme = req.body.theme || '';
-  if (theme.trim().length === 0) {
-    res.status(400).json({
-      error: {
-        message: "Please enter a valid theme for the email subject line",
-      }
-    });
-    return;
-  }
+  const { audience, subjectLine, tone, emailCopy, numLines } = req.body;
+
+  // Add any validation for inputs here...
 
   try {
+    const prompt = `
+    Given these examples of email subject lines:
+    1. Join Wipro and Edgile at the 2022 cyber security summit
+    2. Join Wipro at CS4CA
+    3. Join Wipro and Edgile at Navigate 2022
+    4. Join us for a virtual bourban and bbq event
+    5. Join us at Indianapolis Speedway Save the date - May 16
+    6. Join Wipro and Edgile for a private dinner
+    7. Edgile and Wipro at the Gartner IAM Summit 2023
+    8. Join Wipro, Edgile, and Partners for happy hour!
+    9. Join Wipro at ZenithLive. Discover how we secure the modern enterprise
+    10. Prepare your operational resiliency for changing rules and evolving threats
+    11. Why strategic OT security investments enhance operational resiliency
+    12. Join us at RSAC 2023. Learn how we secure and defend the modern enterprise
+
+    any mention of edgile should be capitalized as "Edgile" and Wipro is always capitalized as "Wipro"
+
+    I want you to act as an expert in generating email subject lines using the examples given above. You should write the subject lines for "${subjectLine}". 
+    The given audience will be "${audience}", it will have a "${tone}" tone and have the email copy of "${emailCopy}". You will generate ${numLines} email subject lines:
+    `;
+
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: generatePrompt(theme),
-      temperature: 0.6,
-      max_tokens: 60,
+      prompt: prompt,
+      temperature: 0.1,
+      max_tokens: 60 * numLines,
     });
+
     res.status(200).json({ result: completion.data.choices[0].text });
-  } catch(error) {
+  } catch (error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
       console.error(error.response.status, error.response.data);
@@ -43,12 +59,8 @@ export default async function (req, res) {
       res.status(500).json({
         error: {
           message: 'An error occurred during your request.',
-        }
+        },
       });
     }
   }
-}
-
-function generatePrompt(theme) {
-  return `An engaging email subject line for a newsletter about ${theme} would be:`;
 }
--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,12 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [themeInput, setThemeInput] = useState("");
-  const [result, setResult] = useState();
+  const [audienceInput, setAudienceInput] = useState("");
+  const [subjectLineInput, setSubjectLineInput] = useState("");
+  const [toneInput, setToneInput] = useState("");
+  const [emailCopyInput, setEmailCopyInput] = useState("");
+  const [numLinesInput, setNumLinesInput] = useState("");
+  const [result, setResult] = useState([]);
 
   async function onSubmit(event) {
     event.preventDefault();
@@ -14,7 +18,13 @@ export default function Home() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ theme: themeInput }),
+        body: JSON.stringify({
+          audience: audienceInput,
+          subjectLine: subjectLineInput,
+          tone: toneInput,
+          emailCopy: emailCopyInput,
+          numLines: numLinesInput,
+        }),
       });
 
       const data = await response.json();
@@ -22,9 +32,8 @@ export default function Home() {
         throw data.error || new Error(`Request failed with status ${response.status}`);
       }
 
-      setResult(data.result);
-      setThemeInput("");
-    } catch(error) {
+      setResult(data.result.trim().split("\n"));
+    } catch (error) {
       // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
@@ -34,23 +43,36 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
+        <title>Email Subject Line Generator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h3>Generate an email subject line</h3>
+        <h3>Generate an Email Subject Line</h3>
         <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="theme"
-            placeholder="Enter a theme for the subject line"
-            value={themeInput}
-            onChange={(e) => setThemeInput(e.target.value)}
-          />
-          <input type="submit" value="Generate subject line" />
+          <input type="text" name="audience" placeholder="Enter audience" value={audienceInput} onChange={(e) => setAudienceInput(e.target.value)} />
+          <input type="text" name="subjectLine" placeholder="Enter proposed subject line" value={subjectLineInput} onChange={(e) => setSubjectLineInput(e.target.value)} />
+          <select name="tone" value={toneInput} onChange={(e) => setToneInput(e.target.value)}>
+            <option value="">--Select Tone--</option>
+            <option value="fun">Fun</option>
+            <option value="corporate">Corporate</option>
+            <option value="fun and corporate">Fun and Corporate</option>
+          </select>
+          <textarea name="emailCopy" placeholder="Enter email copy" value={emailCopyInput} onChange={(e) => setEmailCopyInput(e.target.value)} />
+          <input type="number" name="numLines" placeholder="Enter number of subject lines" value={numLinesInput} onChange={(e) => setNumLinesInput(e.target.value)} />
+          <input type="submit" value="Generate subject lines" />
         </form>
-        <div className={styles.result}>{result}</div>
+        <ul className={styles.result}>
+          {result.map((subjectLine, index) => <li key={index}>{subjectLine}</li>)}
+        </ul>
+        <div className={styles.explanation}>
+          <h4>How This Works</h4>
+          <p>
+            This tool uses OpenAI's GPT-4, a large language model, to generate creative and engaging email subject lines based on the inputs you provide: the intended audience, the desired tone, and the email copy. Under the hood, we use OpenAI's API and give it specific instructions on what to do. 
+            <br />
+            It's important to understand that our code is simply instructing the AI model and that you can do all of this yourself by visiting openai.com and using their APIs. AI isn't replacing us, but marketers that use AI will have an advantage over those who don't. To learn more about AI, visit https://platform.openai.com/
+          </p>
+        </div>
       </main>
     </div>
   );
