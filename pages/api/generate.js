39732ed01b82ca73630943d692a94a7354eba0b0
carlsond2 <89944973+carlsond1@users.noreply.github.com>
2023-06-13 22:15:16 -0400

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -17,7 +17,7 @@ export default async function (req, res) {
 
   const { audience, subjectLine, tone, emailCopy, numLines } = req.body;
 
-  // Add any validation for inputs here...
+  // Add any validation for inputs here...ooo
 
   try {
     const prompt = `
